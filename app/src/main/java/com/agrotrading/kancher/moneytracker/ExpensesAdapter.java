b977ae843965bbc86a93f9c8e6fe90944debcaaa
KaNcHeR <tmnkach@mail.ru>
2015-12-05 14:31:57 +0500

    Adding random background color for filed list

--- app/src/main/java/com/agrotrading/kancher/moneytracker/ExpensesAdapter.java
+++ app/src/main/java/com/agrotrading/kancher/moneytracker/ExpensesAdapter.java
@@ -1,13 +1,16 @@
 package com.agrotrading.kancher.moneytracker;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.List;
+import java.util.Random;
 
 
 public class ExpensesAdapter extends ArrayAdapter {
@@ -24,10 +27,17 @@ public class ExpensesAdapter extends ArrayAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
 
         Expense expense = (Expense) getItem(position);
+        Random rnd = new Random();
+        int bgColor;
 
         if(convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
         }
+
+        RelativeLayout field = (RelativeLayout) convertView.findViewById(R.id.field_item);
+
+        bgColor = Color.argb(40, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+        field.setBackgroundColor(bgColor);
 
         TextView name = (TextView) convertView.findViewById(R.id.name_text);
         TextView sum = (TextView) convertView.findViewById(R.id.sum_text);
